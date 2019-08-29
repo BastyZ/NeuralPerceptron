91931a2ee22602288d988c38310f5eabbdd18f75
basty <bastian.inostroza@gmail.com>
2019-08-28 21:22:39 -0400

    implemented learning tests on the Neuron

--- test/assignment1/NeuronTest.kt
+++ test/assignment1/NeuronTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import kotlin.math.abs
 import kotlin.random.Random
 
 internal class NeuronTest {
@@ -18,28 +19,28 @@ internal class NeuronTest {
     private val initialBias: Double = 0.0
     private val testingSample = createPoints(testingPoints)
     private val trainingSample = createPoints(trainingPoints)
-    private val inputs00 = listOf(0.0,0.0)
-    private val inputs01 = listOf(0.0,1.0)
-    private val inputs10 = listOf(1.0,0.0)
-    private val inputs11 = listOf(1.0,1.0)
+    private val inputs00 = listOf(0.0, 0.0)
+    private val inputs01 = listOf(0.0, 1.0)
+    private val inputs10 = listOf(1.0, 0.0)
+    private val inputs11 = listOf(1.0, 1.0)
 
     lateinit var neuron: Neuron
 
     private fun overLine(x: Double, y: Double): Double {
         // Line y = -x - 4
         return when {
-            -x - 4.0 > y  -> 1.0
-            else        -> 0.0
+            -x - 4.0 > y -> 1.0
+            else -> 0.0
         }
     }
 
     private fun createPoints(nOfPoints: Int): Array<Pair<List<Double>, Double>> {
         // Creates a list of points and the desired output for each one
         return Array(nOfPoints) {
-            val x: Double = Random.nextInt(-width,width).toDouble()
-            val y: Double = Random.nextInt(-height,height).toDouble()
+            val x: Double = Random.nextInt(-width, width).toDouble()
+            val y: Double = Random.nextInt(-height, height).toDouble()
             val desiredOutput: Double = overLine(x, y)
-            Pair(listOf(x,y), desiredOutput)
+            Pair(listOf(x, y), desiredOutput)
         }
     }
 
@@ -47,7 +48,7 @@ internal class NeuronTest {
     fun setUp() {
         // If nothing is made we start the test with a neuron
         // with bias 0.0 and weights 1.0 and Sigmoid activation fun
-        neuron = Neuron(initialWeights, initialBias, Sigmoid())
+        neuron = Neuron(initialWeights, initialBias, Sigmoid(), 0.1)
     }
 
     @Test
@@ -78,6 +79,65 @@ internal class NeuronTest {
     }
 
     @Test
-    fun train() {
+    fun spaceSigmoidNoTrains() {
+        var successfulTries = 0
+        // predicts for all the training sample
+        testingSample.forEach {
+            when {
+                abs( it.second - neuron.feed(it.first) ) < .05 -> successfulTries++
+            }
+        }
+        val successRate: Double = successfulTries.toDouble() / testingPoints
+        println("No training :: SuccessRate is $successRate")
+        // with no training we expect to perform almost like random
+        assertTrue(successRate < .6)
+    }
+
+    @Test
+    fun spaceSigmoidTrainOnce() {
+        // train the neuron with the training sample
+        // error is not computed using derivatives (yet, we'll use it on the network)
+        trainingSample.forEach {
+            val output = neuron.feed(it.first)
+            neuron.adjustDeltaWith(it.second - output, output)
+            neuron.train(it.first, it.second)
+        }
+
+        // we use another sample on the same line to test the result
+        var successfulTries: Int = 0
+        testingSample.forEach {
+            when {
+                abs(it.second - neuron.feed(it.first)) < .05 -> successfulTries++
+            }
+        }
+        val successRate: Double = successfulTries.toDouble() / testingPoints
+        println("One training :: SuccessRate is $successRate")
+        // we expect this to be better than random
+        assert(successRate > .5)
+    }
+
+    @Test
+    fun spaceSigmoidTrainN(){
+        // train the neuron with the training sample various times
+        // error is not computed using derivatives (yet, we'll use it on the network)
+        repeat(trainingSessions) {
+            trainingSample.forEach {
+                val output = neuron.feed(it.first)
+                neuron.adjustDeltaWith(it.second - output, output)
+                neuron.train(it.first, it.second)
+            }
+        }
+
+        // we use another sample on the same line to test the result
+        var successfulTries = 0
+        testingSample.forEach {
+            when {
+                abs( it.second - neuron.feed(it.first) ) < .05 -> successfulTries++
+            }
+        }
+        val successRate: Double = successfulTries.toDouble() / testingSample.size
+        print("$trainingSessions Trainings :: SuccessRate is $successRate \n")
+        // We expect it to perform very well with various rounds of training
+        assertTrue(successRate > 0.8)
     }
 }
