073bf34a29e917651d552ca3dcf7d4f8341fd23c
basty <bastian.inostroza@gmail.com>
2019-12-06 21:30:06 -0300

    added "meat" to the node class

--- src/assignment3/Node.kt
+++ src/assignment3/Node.kt
@@ -1,4 +1,33 @@
 package assignment3
 
-class Node {
+data class Node(var function: (Node, Node) -> Unit, var father: Node? = null) {
+    // la clase recibe dos argumentos y retorna uno
+    private var arguments: MutableList<Node> = mutableListOf<Node>()
+    private var numArguments = 2
+
+    fun eval() {
+        check( arguments.size == numArguments ) {"La cantidad de argumentos no calza con la de argumentos"}
+        return this.function(arguments.first(), arguments.last())
+    }
+
+    fun serialize(): MutableList<Any> {
+        var list: MutableList<Any> = mutableListOf(this)
+        for (node in this.arguments) {
+            list.add(node.serialize())
+        }
+        return list
+    }
+
+    fun copy(): Node{
+        // TODO add deepcopy
+        val aNode = Node(this.function, this.father)
+        aNode.arguments = this.arguments
+        return aNode
+    }
+
+    fun replace(otherNode: Node) {
+        this.function = otherNode.function
+        this.arguments = otherNode.arguments
+        this.numArguments = otherNode.numArguments
+    }
 }
