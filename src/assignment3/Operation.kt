911a14bd16ac4e5be1bbfda5e8ed855c0bbe868d
basty <bastian.inostroza@gmail.com>
2019-12-09 23:01:39 -0300

    completed and fix'd all functions, and added a little of documentation

--- src/assignment3/Operation.kt
+++ src/assignment3/Operation.kt
@@ -2,6 +2,15 @@ package assignment3
 
 import kotlin.math.max
 
+/**
+ *  Using the function-type interface as shown here:
+ *    https://github.com/JetBrains/kotlin/blob/master/spec-docs/function-types.md
+ *
+ *  And overriding 'toString()' to make it look pretty after
+ *
+ *  PS: I did'nt want and try to do it for N nodes, sounds too messy for the function types
+ */
+
 class addFun(val a: Node, val b: Node): (Node, Node) -> Int {
     override fun invoke(a: Node, b: Node): Int = a.eval() + b.eval()
     override fun toString(): String = "${this.a} + ${this.b}"
@@ -17,20 +26,18 @@ class maxFun(val a: Node, val b: Node): (Node, Node) -> Int {
     override fun toString(): String = "max(${this.a}, ${this.b})"
 }
 
-class minFun(a: Node, b: Node): (Node, Node) -> Int {
-    // Todo: terminar
+class minFun(val a: Node, val b: Node): (Node, Node) -> Int {
     override fun invoke(a: Node, b: Node): Int = a.eval() + b.eval()
-    override fun toString(): String = "+"
+    override fun toString(): String = "min(${this.a}, ${this.b})"
 }
 
-class multFun(a: Node, b: Node): (Node, Node) -> Int {
-    // Todo: terminar
+class multFun(val a: Node, val b: Node): (Node, Node) -> Int {
     override fun invoke(a: Node, b: Node): Int = a.eval() + b.eval()
-    override fun toString(): String = "+"
+    override fun toString(): String = "${this.a} * ${this.b}"
 }
 
-class divFun(a: Node, b: Node): (Node, Node) -> Int {
-    // Todo: terminar
+class divFun(val a: Node, val b: Node): (Node, Node) -> Int {
+    // Todo: hacer caso division cero
     override fun invoke(a: Node, b: Node): Int = a.eval() + b.eval()
-    override fun toString(): String = "+"
+    override fun toString(): String = "${this.a} / ${this.b}"
 }
