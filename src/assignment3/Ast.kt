1f64a5c1b00510c0f0e213c26ae85b788d63cd84
basty <bastian.inostroza@gmail.com>
2019-12-10 21:33:37 -0300

    created AST from python material

--- src/assignment3/Ast.kt
+++ src/assignment3/Ast.kt
@@ -1,4 +1,36 @@
 package assignment3
 
-class Ast {
+import kotlin.random.Random
+
+class Ast(
+    val allowedFunctions: MutableList<(Node, Node) -> Int>,
+    internal val allowedTerminalValues: MutableList<Int>,
+    val terminalNodeProbability: Double = 0.3
+) {
+    fun invoke(maxDepth: Int): Node {
+        // aux fun
+         fun createRecTree(depth:Int): Node {
+            when {
+                depth > 0 -> {
+                    // we choose a random function
+                    val nodeFun = allowedFunctions.random()
+                    // we create it's children randomly
+                    val children: List<Node> = List(2) {
+                        // there is a possibility of don't keep creating sub-trees
+                        // for the entire eternity
+                        when (Random.nextInt() < terminalNodeProbability) {
+                            true -> createRecTree(0)
+                            false -> createRecTree(depth - 1)
+                        }
+                    }
+
+                    // we do not have (I think) unpacking here, be we know they're only two
+                    return BinaryNode(nodeFun, children.first(), children.last())
+                }
+                else -> return TerminalNode(value = allowedTerminalValues.random())
+            }
+        }
+
+        return createRecTree(maxDepth)
+    }
 }
